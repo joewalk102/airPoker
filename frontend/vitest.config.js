db7aebd0952c7a0f69522211d0caa18f25f815d5
Joe Walker <joewalk102@gmail.com>
2026-05-29 08:46:16 -0700

    Add coverage config

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -7,6 +7,18 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/vitest.setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.d.ts',
+        'src/index.tsx',
+        'src/App.tsx',
+        'src/reportWebVitals.ts',
+        'src/setupTests.ts',
+      ]
+    }
   },
 });
 
